b3cd202e936fd103510a244ceb3ec6fb5155799e
unknown <wooae@umich.edu>
2015-12-30 23:25:30 -0500

    about page content and navigation bar

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 app = Flask(__name__, template_folder='views')
 
-@app.route("/")
+@app.route("/papernet")
 def index():
-	return render_template("index.html")
+	page = "home"
+	return render_template("index.html", page = page)
+	
+@app.route("/papernet/about")
+def about():
+	page = "about"
+	return render_template("index.html", page = page)
+	
+@app.route("/papernet/market_research")
+def market():
+	page = "market"
+	return render_template("index.html", page = page)
 
 if __name__== '__main__':
 	app.run(host = 'localhost', port = 3000, debug = True)
