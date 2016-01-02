66fdc74b5770a26c14d06300f15946b4e29861d0
unknown <wooae@umich.edu>
2016-01-02 10:46:27 -0500

    added paths and market research content

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
+# from flaskext.mail import Mail, Message
+
 app = Flask(__name__, template_folder='views')
+# mail = Mail(app)
 
 @app.route("/papernet")
 def index():
@@ -21,10 +24,20 @@ def features():
 	page = "features"
 	return render_template("index.html", page = page)
 	
-@app.route("/papernet/contact")
+@app.route("/papernet/contact", methods = ['GET', 'POST'])
 def contact():
 	page = "contact"
-	return render_template("index.html, page = page")
+	message_sent = false
+#	if request.method == 'POST':
+#		sender_name = request.form['name']
+#		sender_email = request.form['email']
+#		content = request.form['message']
+#		msg = Message("New message from the Papernet of Things website!", sender = (sender_name, sender_email), recipients = ["wooae@umich.edu"])
+#		assert msg.sender == (sender_name + " <" + sender_email + ">")
+#		msg.body = content
+#		mail.send(msg)
+#		message_sent = true 
+	return render_template("index.html", page = page, message_sent = message_sent)
 
 if __name__== '__main__':
 	app.run(host = 'localhost', port = 3000, debug = True)
